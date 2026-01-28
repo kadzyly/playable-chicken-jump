ea186f30734cf00517c50b14181f85b1b2265cb6
KadzyLy <kadzyly@gmail.com>
2026-01-28 07:02:49 +0100

    docs: add todo

--- src/entities/StartIsland.ts
+++ src/entities/StartIsland.ts
@@ -4,5 +4,7 @@ export class StartIsland extends PIXI.Sprite {
   constructor() {
     super(PIXI.Assets.get('startIsland'));
     this.anchor.set(0.5, 0.5);
+    // TODO set new pivot for adaptive
+    // island.pivot.set(450, 100);
   }
 }
