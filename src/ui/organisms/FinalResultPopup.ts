2ca7dea132ed478fbe5c0d2406420b6ce9d128c1
KadzyLy <kadzyly@gmail.com>
2026-01-30 03:50:26 +0100

    feat: set styles for win popup

--- src/ui/organisms/FinalResultPopup.ts
+++ src/ui/organisms/FinalResultPopup.ts
@@ -12,7 +12,14 @@ export class FinalResultPopup extends PopupUI {
       align: 'center'
     });
 
-    const line1 = new Text({ text: `New year`, style });
+    const style2 = new TextStyle({
+      fontFamily: 'Marvin400, Arial',
+      fontSize: 32,
+      fill: 0xffe40f,
+      align: 'center'
+    });
+
+    const line1 = new Text({ text: `New year`, style: style2 });
     line1.anchor.set(0.5);
     line1.y = -40;
 
@@ -20,7 +27,7 @@ export class FinalResultPopup extends PopupUI {
     line2.anchor.set(0.5);
     line2.y = 0;
 
-    const line3 = new Text({ text: `+$3,760.0`, style });
+    const line3 = new Text({ text: `+$3,760.0`, style: style2 });
     line3.anchor.set(0.5);
     line3.y = 40;
 
