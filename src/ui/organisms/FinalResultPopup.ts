19f666d3a75304ae6eef78a17368c62d2a46b809
KadzyLy <kadzyly@gmail.com>
2026-01-30 15:08:12 +0100

    feat: styles win popup

--- src/ui/organisms/FinalResultPopup.ts
+++ src/ui/organisms/FinalResultPopup.ts
@@ -7,29 +7,30 @@ export class FinalResultPopup extends PopupUI {
 
     const style = new TextStyle({
       fontFamily: 'Marvin400, Arial',
-      fontSize: 32,
+      fontSize: 70,
       fill: 0xffffff,
       align: 'center'
     });
 
     const style2 = new TextStyle({
       fontFamily: 'Marvin400, Arial',
-      fontSize: 32,
+      fontSize: 76,
       fill: 0xffe40f,
       align: 'center'
     });
 
     const line1 = new Text({ text: `New year`, style: style2 });
-    line1.anchor.set(0.5);
-    line1.y = -40;
-
     const line2 = new Text({ text: `Jackpot!`, style });
-    line2.anchor.set(0.5);
-    line2.y = 0;
+    const line3 = new Text({ text: `+3,760.0€`, style: style2 });
 
-    const line3 = new Text({ text: `+€3,760.0`, style: style2 });
+    line1.anchor.set(0.5);
+    line1.y = -76;
+
+    line2.anchor.set(0.5);
+    line2.y = 8;
+
     line3.anchor.set(0.5);
-    line3.y = 40;
+    line3.y = 96;
 
     this.panel.addChild(line1, line2, line3);
   }
