7a4e78c53d8e55992923cc882d7ff5082e4ed229
KadzyLy <kadzyly@gmail.com>
2026-02-02 15:51:47 +0100

    feat: add screen aadapter

--- src/Game.ts
+++ src/Game.ts
@@ -4,6 +4,7 @@ import { loadAssets } from './core/Assets';
 import { SoundManager } from './core/SoundManager';
 import { MainScene } from './scene/MainScene';
 import { loadFonts } from './core/Fonts';
+import { ScreenAdapter } from './core/ScreenAdapter';
 
 export class Game {
   private app!: PIXI.Application;
@@ -16,12 +17,19 @@ export class Game {
   public resize(width: number, height: number): void {
     if (!this.app || !this.scene) return;
 
+    const screenAdapter = ScreenAdapter.getInstance();
+    screenAdapter.updateDimensions(width, height);
+
     this.app.renderer.resize(width, height);
-    this.scene.resize(width, height);
+    this.scene.resize(width, height, screenAdapter.isLandscape());
   }
 
   private async init(width: number, height: number): Promise<void> {
     this.app = await createApp(width, height);
+    
+    const screenAdapter = ScreenAdapter.getInstance();
+    screenAdapter.updateDimensions(width, height);
+    
     await loadAssets();
     await loadFonts();
 
